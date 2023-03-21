b47ef13055dcc49c50d7663f0e4258ec512a4f46
changgohap <changgohap@gmail.com>
2023-03-21 17:18:28 +0900

    #front/searchresult [newslist 구현[완], filter 구현 [진행중]

--- frontend/src/pages/SearchResultPage/SearchResult.jsx
+++ frontend/src/pages/SearchResultPage/SearchResult.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import NewsList from 'components/Common/News/NewsList';
-import Category from 'components/Common/News/Category';
 import styles from './SearchResult.module.scss';
 
 export default function SearchResult() {
   return (
     <div className={styles['searchresult']}>
-      <Category />
       <NewsList />
     </div>
   );
