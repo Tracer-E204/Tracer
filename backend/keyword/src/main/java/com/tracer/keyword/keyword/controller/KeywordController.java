7ccd14028a7adf2a931bab049d052c7d2a82631e
rudgns9334 <rudgns9334@gmail.com>
2023-03-23 18:00:15 +0900

    #S08P22E204-91 #done #comment [데일리키워드]

--- backend/keyword/src/main/java/com/tracer/keyword/keyword/controller/KeywordController.java
+++ backend/keyword/src/main/java/com/tracer/keyword/keyword/controller/KeywordController.java
@@ -1,18 +1,26 @@
 package com.tracer.keyword.keyword.controller;
 
+import com.tracer.keyword.keyword.service.DailyService;
+import com.tracer.keyword.keyword.vo.ResDailyKeyword;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/keyword")
 public class KeywordController {
-    @GetMapping("/")
-    public ResponseEntity<Object> test(){
-        return ResponseEntity.status(HttpStatus.OK).body("hello keyword");
+
+    private final DailyService dailyService;
+    @GetMapping("/daily")
+    public ResponseEntity<Object> dailyKeyword(@RequestParam("type") Integer type){
+        List<ResDailyKeyword> list = dailyService.dailyKeyword(type);
+        return ResponseEntity.status(HttpStatus.OK).body(list);
     }
 }
