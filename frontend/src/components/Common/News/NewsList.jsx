cb9e798c0b2df4109642d2e0ffe0d856cd749413
changgohap <changgohap@gmail.com>
2023-03-23 17:48:13 +0900

    #feature/front/login: login layout[구현중] & searchbar data 넣기 [구현중]

--- frontend/src/components/Common/News/NewsList.jsx
+++ frontend/src/components/Common/News/NewsList.jsx
@@ -1,33 +1,17 @@
-import React from 'react';
-import { useQuery } from '@tanstack/react-query';
-import { searchNewsList } from 'apis/api/api';
+import React, { useState } from 'react';
 import NewsItem from './NewsItem';
 import styles from './NewsList.module.scss';
 import Category from 'components/Common/News/Category';
-import { news } from './news';
 
 // 카테고리를 props로 받아옴
-export default function NewsList() {
-  const params = JSON.stringify({
-    word: '러시아',
-    limit: 5,
-    offset: 0,
-  });
-  const { data, isLoading } = useQuery({
-    queryKey: ['newsList'],
-    queryFn: () => searchNewsList(params),
-  });
-
-  if (isLoading) return;
-
-  // console.log(data);
-
+export default function NewsList(result) {
+  console.log(result);
   return (
     <div className={styles.newsList}>
-      {/* <Category />
-      {data.map(n => (
-        <NewsItem key={n.news_reporter} article={n} />
-      ))} */}
+      <Category />
+      {result.list.map(n => (
+        <NewsItem key={n.newsReporter} article={n} />
+      ))}
     </div>
   );
 }
