40ff69b79cd8cfc830db14f1b25c65ebcc23148f
leeflection <leeflection@gmail.com>
2023-03-24 00:12:21 +0900

    #S08P22E204-64 #comment 수정 사항

--- backend/etl/main.py
+++ backend/etl/main.py
@@ -112,7 +112,7 @@ def dump():
 
     result = []
     hmap = ['society', 'economic', 'foreign', 'digital', 'culture']
-    start = "2023-01-16"
+    start = "2023-01-26"
     last = "2023-03-23"
     start_date = datetime.strptime(start, "%Y-%m-%d")
     last_date = datetime.strptime(last, "%Y-%m-%d")
