a58a83f06e0e84b5de211b0616a48d0e081f7d13
rudgns9334 <rudgns9334@gmail.com>
2023-03-27 16:42:36 +0900

    #noid #done #comment [3줄요약 완]

---
+++ backend/news/src/main/java/com/tracer/news/news/entity/Shortcut.java
@@ -0,0 +1,30 @@
+package com.tracer.news.news.entity;
+
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamicUpdate
+@DynamicInsert
+public class Shortcut {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "shortcut_id")
+    private Long shortcutId;
+
+    @Column(name = "content_1st")
+    private String content1st;
+    @Column(name = "content_2nd")
+    private String content2nd;
+    @Column(name = "content_3rd")
+    private String content3rd;
+
+}
