883b26c1ffc77a516b8020396b5553481ad141d8
changgohap <changgohap@gmail.com>
2023-03-27 17:50:00 +0900

    #noid #Mainpage (search, newsslide) 마무리[진행 중]

--- frontend/src/components/MainPage/SearchPage.jsx
+++ frontend/src/components/MainPage/SearchPage.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import styles from './SearchBar.module.scss';
+import styles from './SearchPage.module.scss';
 import SearchBar from 'components/Common/SearchBar';
+import video from './assets/SearchPageBackground.mp4';
 
 export default function SearchPage() {
   return (
     <div className={styles['searchbar-container']}>
-      <SearchBar />
+      <video autoPlay muted>
+        <source src={video} type="video/mp4" />
+      </video>
+      <div className={styles['search-component']}>
+        <h1> 관심있는 키워드를 검색하고 </h1>
+        <h3> 뉴스의 흐름을 알아보세요! </h3>
+        <SearchBar className={styles['searchbar']} />
+      </div>
     </div>
   );
 }
