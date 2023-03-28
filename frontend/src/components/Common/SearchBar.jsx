4578ec9d32c77a06d2c1c60ac57cc517fd221c02
이승민 <curiously12@naver.com>
2023-03-28 15:31:47 +0900

    feature/frontend/Pagination, Category

--- frontend/src/components/Common/SearchBar.jsx
+++ frontend/src/components/Common/SearchBar.jsx
@@ -1,12 +1,9 @@
 import React, { useState } from 'react';
-import styles from './SearchBar.module.scss';
+// import styles from './SearchBar.module.scss';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 export default function SearchBar() {
-  // post 요청 후 뉴스 리스트 저장할 변수
-  const [result, setResult] = useState(null);
-
   // search bar 내에 들어갈 검색어 변수
   const [text, setText] = useState('');
 
@@ -20,14 +17,13 @@ export default function SearchBar() {
   const handleSubmit = async e => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://200.0.0.60/news/search', {
+      const response = await axios.post('http://j8e204.p.ssafy.io:8001/news/search', {
         word: text,
         limit: 5,
         offset: 0,
         type: 0,
       });
-      setResult(response.data);
-      navigate(`/searchresult`, { state: { result: response.data } });
+      navigate(`/searchresult`, { state: { result: response.data, text: text } });
     } catch (error) {
       console.error('Error sending POST request:', error);
     }
