96124b843848dee957c931ee06bf03e769750065
rudgns9334 <rudgns9334@gmail.com>
2023-03-29 17:54:17 +0900

    #noid #done #comment [keyword, timeline shutdown]

--- backend/timeline/src/main/java/com/tracer/timeline/timeline/controller/TimelineController.java
+++ backend/timeline/src/main/java/com/tracer/timeline/timeline/controller/TimelineController.java
@@ -4,13 +4,15 @@ import com.tracer.timeline.timeline.service.TimelineService;
 import com.tracer.timeline.timeline.vo.ResTimeline;
 import com.tracer.timeline.timeline.vo.ResTimelineClusterList;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -19,6 +21,18 @@ import java.util.List;
 public class TimelineController {
 
     private final TimelineService timelineService;
+
+    @GetMapping("/shutdown")
+    public ResponseEntity<String> actuator(HttpServletRequest request) {
+        String url = request.getServerName() + ":" + request.getServerPort();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        HttpEntity<MultiValueMap<String, String>> req = new HttpEntity<>(headers);
+        RestTemplate rt = new RestTemplate();
+        ResponseEntity<String> res = rt.exchange("http://"+ url + "/actuator/shutdown", HttpMethod.POST,req,String.class);
+//        ResponseEntity<String> res = rt.exchange("http://"+ url + "/actuator", HttpMethod.GET,null,String.class);
+        return res;
+    }
 
     @GetMapping("/{timelineId}")
     public ResponseEntity<Object> selectTimeline(@PathVariable Long timelineId){
