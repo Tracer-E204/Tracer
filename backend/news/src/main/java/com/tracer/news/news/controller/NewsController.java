96e5603e7409656b5a0817aeaa066d858362396f
rudgns9334 <rudgns9334@gmail.com>
2023-04-04 13:38:50 +0900

    다시 api 추가

--- backend/news/src/main/java/com/tracer/news/news/controller/NewsController.java
+++ backend/news/src/main/java/com/tracer/news/news/controller/NewsController.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/news")
+@RequestMapping("/api/news")
 public class NewsController {
 
     private final NewsService newsService;
