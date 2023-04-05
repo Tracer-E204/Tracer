e113c5afb07460e0d60eca78bca9d83aa20bc17e
leeflection <leeflection@gmail.com>
2023-04-05 13:31:59 +0900

    S08P22E204-163 #in-gress #comment 날짜수정

--- backend/etl/main.py
+++ backend/etl/main.py
@@ -112,8 +112,8 @@ def dump():
 
     result = []
     hmap = ['society', 'economic', 'foreign', 'digital', 'culture']
-    start = "2023-03-23"
-    last = "2023-04-03"
+    start = "2023-04-03"
+    last = "2023-04-04"
     start_date = datetime.strptime(start, "%Y-%m-%d")
     last_date = datetime.strptime(last, "%Y-%m-%d")
     while start_date <= last_date:
