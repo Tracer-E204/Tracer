686bcaa0a1fb766aa040b2e4c67a0d9d146ac722
이승민 <curiously12@naver.com>
2023-04-05 16:12:50 +0900

    feature/front/Filter

--- frontend/src/pages/SearchResultPage/SearchResult.jsx
+++ frontend/src/pages/SearchResultPage/SearchResult.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import NewsList from 'components/Common/News/NewsList';
 import styles from './SearchResult.module.scss';
@@ -7,11 +7,44 @@ import Filter from 'components/Common/News/Filter';
 export default function SearchResult() {
   const location = useLocation();
   const { state } = location;
+  const [result, setResult] = useState(null);
+  const [startDate, setStartDt] = useState(null);
+  const [EndDate, setEndDt] = useState(null);
+  const [index, setIndex] = useState(false);
+  const handleDate = (startDate, EndDate) => {
+    setStartDt(startDate);
+    setEndDt(EndDate);
+  };
+  const handleApply = data => {
+    setResult(data);
+  };
+  if (result === null) {
+    setResult(state.result);
+    setStartDt(state.startDate);
+    setEndDt(state.EndDate);
+  }
   window.scrollTo(0, 0);
+  const handleIndex = tf => {
+    setIndex(tf);
+  };
   return (
     <div className={styles.searchresult}>
-      <Filter />
-      <NewsList result={state.result} text={state.text} />
+      <Filter
+        text={state.text}
+        onApply={handleApply}
+        onDate={handleDate}
+        startDt={startDate}
+        endDt={EndDate}
+        setIndex={handleIndex}
+      />
+      <NewsList
+        result={result}
+        text={state.text}
+        startDt={startDate}
+        endDt={EndDate}
+        index={index}
+        setIndex={handleIndex}
+      />
     </div>
   );
 }
